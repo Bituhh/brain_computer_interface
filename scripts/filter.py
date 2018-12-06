6ae9603c677723b09bfca0941ed144e111d11184
bituhh <victor@oliveira.org.uk>
2018-12-06 19:16:17 +0000

    eeg signal been read and displayed - ToDo: Add all other datasets

--- scripts/filter.py
+++ scripts/filter.py
@@ -1,4 +1,25 @@
 import numpy as np
+from scipy.signal import butter
+import mne
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
-    pass
+    
+    #montage = mne.channels.read_montage('standard_1020')
+    #print(montage.ch_names)
+
+    # Initialising Parameters
+    raw = mne.io.read_raw_edf('../dataset/BCICIV_2b_gdf/B0101T.gdf', preload=True)
+    data = raw.get_data()
+    #montage = mne.channels.read_montage("standard_1020")
+    events = mne.find_events(raw, initial_event=True)
+    print(events)
+    
+    # Setting Parameters
+    #raw.set_montage(montage)
+    #raw.set_eeg_reference("average")
+    
+    # Show plot
+    #plt.plot(raw.get_data[-1])
+    raw.plot(block=True, events=events)
